d80e663cfb9314214d7c0b9ce2af4efb4ae443ab
juanito16 <juan_alexander11@hotmail.com>
2013-09-24 00:15:49 -0600

    modificando conexion de base de datos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SistemaControlSalitasV1::Application.config.secret_key_base = '8536feadbd6415f35cf8d8685d39ea49f5a5e25ee412d8c631f7be95a2087b777fe5528bcec13a6aad0c1bc778e1e63527e7e45e6776a947b9cd27847c593584'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SistemaControlSalitasV1::Application.config.session_store :cookie_store, key: '_sistema_control_salitas_v1_session'
