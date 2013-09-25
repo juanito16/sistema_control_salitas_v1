bbbcdd217e3ba48cb24522652fa1166f39731084
juanito16 <juan_alexander11@hotmail.com>
2013-09-24 22:31:28 -0600

    modulo agregando compañias

--- Gemfile
+++ Gemfile
@@ -43,3 +43,8 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SistemaControlSalitasV1::Application.routes.draw do
+  resources :companies
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
