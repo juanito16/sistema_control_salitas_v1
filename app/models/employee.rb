943a7329e7ed56fa0b89eca33a67cb68f8fea860
juan_alexander11@hotmail.com <juan_alexander11@hotmail.com>
2013-09-27 10:08:50 -0600

    modificando agregar_compañia

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,6 @@ class Employee < ActiveRecord::Base
   belongs_to :company
   belongs_to :country
   mount_uploader :photo, EmployeeUploader
-  self.per_page = 10
-
+  self.per_page = 10	
+  validates :name,:lastname,:email,:company_id,:country_id,:photo, presence: true
 end
