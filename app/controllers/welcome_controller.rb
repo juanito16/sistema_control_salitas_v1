329d02f7928fa5da21413b9ebb8db9cf566001cd
juanito16 <juan_alexander11@hotmail.com>
2013-10-10 21:38:03 -0600

    modificando vistas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@active_home="active"
   	@parlos = Parlo.where("country_id=?",1)
+  	@reservations=Reservation.all
   	@reservation = Reservation.new
   end
 end
