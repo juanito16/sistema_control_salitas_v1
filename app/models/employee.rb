bed206af5529b10fecbe9eba4cd3bd16f3be0b32
juanito16 <juan_alexander11@hotmail.com>
2013-10-14 20:13:28 -0600

    modificando diseño reservacion salitas

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,5 @@ class Employee < ActiveRecord::Base
   belongs_to :country
   has_many :reservations
   mount_uploader :photo, EmployeeUploader
-  self.per_page = 10	
   validates :name,:lastname,:email,:company_id,:country_id,:photo, presence: true
 end
