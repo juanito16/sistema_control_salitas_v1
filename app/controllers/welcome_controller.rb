61499a5935ddb378719a2f2e04461d949e2cdb62
juanito16 <juan_alexander11@hotmail.com>
2014-02-10 20:45:33 -0600

    haciendo areglos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,11 @@ class WelcomeController < ApplicationController
   
   def index
   	@active_home="active"
-  	@parlos = Parlo.where("country_id=?",current_admin.country_id)
+    if current_admin.admin_type==1
+  	 @parlos = Parlo.where("country_id=?",current_admin.country_id)
+    else
+      @parlos = Parlo.all
+    end
     #parlos_id=Parlo.all
     @reservation_parlo = Reservation.new
     #@reservasions_ocupation=Reservation.includes(:parlo).where("parlos.id=?",parlos_id.id).references(:parlo).limit(2)
